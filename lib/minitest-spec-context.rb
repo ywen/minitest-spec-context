e05e87a72d141259b51ef059ae69b52e10118e24
Yi Wen <hayafirst@gmail.com>
2012-04-09 21:22:13 -0500

    Initial commit

---
+++ lib/minitest-spec-context.rb
@@ -0,0 +1,9 @@
+require "minitest-spec-context/version"
+
+module Minitest
+  module Spec
+    module Context
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/minitest-spec-context/version.rb
@@ -0,0 +1,7 @@
+module Minitest
+  module Spec
+    module Context
+      VERSION = "0.0.1"
+    end
+  end
+end
