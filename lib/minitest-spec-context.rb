dbb71e9ada4386e56ac4636697ebe436cd0939de
Yi Wen <hayafirst@gmail.com>
2012-04-10 21:01:53 -0500

    Simply alias the context to describe

--- lib/minitest-spec-context.rb
+++ lib/minitest-spec-context.rb
@@ -2,8 +2,8 @@ require 'minitest/spec'
 
 module MiniTest
   class Spec
-    def self.context(desc, additional_desc = nil, &block)
-      describe desc, additional_desc = nil, &block
+    class << self
+      alias_method :context, :describe
     end
   end
 end
--- minitest-spec-context.gemspec
+++ minitest-spec-context.gemspec
@@ -11,5 +11,5 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "minitest-spec-context"
   gem.require_paths = ["lib"]
-  gem.version       = '0.0.2'
+  gem.version       = '0.0.3'
 end
